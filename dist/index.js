389e97e77752b104be123e5af12516041825a191
cyberdantes <36632295+cyberdantes@users.noreply.github.com>
2022-01-27 09:39:24 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -558,8 +558,7 @@ const github = __webpack_require__(469);
 const createBranch = __webpack_require__(503);
 
 async function run() {
-  try {
-    const sourceBranch = core.getInput("SOURCE_BRANCH", { required: true });
+try {
     const targetBranchPattern = core.getInput("TARGET_BRANCH_STARTS_WITH", {
       required: true,
     });
@@ -576,64 +575,13 @@ async function run() {
       ref: `heads/${targetBranchPattern}`,
     });
 
+    const branchNames = [];
     for (let branchData of targetBranches) {
       const branch = branchData.ref.replace("refs/heads/", "");
-      console.log(`Making a pull request for ${branch} from ${sourceBranch}.`);
-      // part of test
-      const { data: currentPulls } = await octokit.pulls.list({
-        owner: repository.owner.login,
-        repo: repository.name,
-      });
-
-      // create new branch from SOURCE_BRANCH and PR between new branch and target branch
-      const context = github.context;
-      const newBranch = `${sourceBranch}-to-${branch}`;
-
-      const currentPull = currentPulls.find((pull) => {
-        console.log(pull.head.ref, pull.base.ref);
-        return pull.head.ref === newBranch && pull.base.ref === branch;
-      });
-
-      if (!currentPull) {
-        await createBranch(octokit, context, newBranch);
-
-        const { data: pullRequest } = await octokit.pulls.create({
-          owner: repository.owner.login,
-          repo: repository.name,
-          head: newBranch,
-          base: branch,
-          title: `sync: ${sourceBranch} to ${branch}`,
-          body: `sync-branches: syncing ${branch} with ${sourceBranch}`,
-          draft: false,
-        });
-
-        console.log(
-          `Pull request (${pullRequest.number}) successful! You can view it here: ${pullRequest.url}.`
-        );
-
-        core.setOutput("PULL_REQUEST_URL", pullRequest.url.toString());
-        core.setOutput("PULL_REQUEST_NUMBER", pullRequest.number.toString());
-      } else {
-        // If PR exists update PR branch with sourceBranch
-        console.log(
-          `There is already a pull request (${currentPull.number}) to ${branch} from ${newBranch}.\n`
-        );
-        console.log("Updating PR branch...");
-
-        await octokit.repos.merge({
-          owner: repository.owner.login,
-          repo: repository.name,
-          base: newBranch,
-          head: context.sha,
-        });
-
-        console.log("PR branch updated\n");
-        console.log(`You can view it here: ${currentPull.url}`);
-
-        core.setOutput("PULL_REQUEST_URL", currentPull.url.toString());
-        core.setOutput("PULL_REQUEST_NUMBER", currentPull.number.toString());
-      }
+      branchNames.push(branch);      
     }
+    
+    core.setOutput("LIST", branchNames.join());
   } catch (error) {
     core.setFailed(error.message);
   }
